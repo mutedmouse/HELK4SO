eba7ff2f1412d60127081c9a750d5ce6446c8fb5
Muted Mouse <mutedmouse@gmail.com>
2019-04-01 14:48:01 -0400

    Added ip validation function

--- host-setup.sh
+++ host-setup.sh
@@ -105,6 +105,24 @@ function restart_logstash()
     echo "Logstash successfully restarted. Now continuing."
 }
 
+#ip address validation function
+#function valid_ip()
+#{
+#    local  ip=$1
+#    local  stat=1
+#
+#    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+#        OIFS=$IFS
+#        IFS='.'
+#        ip=($ip)
+#        IFS=$OIFS
+#        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+#            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+#        stat=$?
+#    fi
+#    return $stat
+#}
+
 #verify run as root
 if [ "$(id -u)" != "0" ]; then
     echo "This script must be run as root" 1>&2
